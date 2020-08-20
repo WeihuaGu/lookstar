78e6e030655a579c4d16c7ec6af4ced65d7ec629
weihmi <ministarmail@msn.cn>
2020-08-20 10:52:49 +0800

    first version

---
+++ build/precache-manifest.7f190e144986a51c484446c5627d5d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a349a182ce006ca4bd7bffff2c8380c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9604c5073fa83e0f4207",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a8cdfaba4377f25267b4",
+    "url": "/static/js/2.09f20dc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09f20dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9604c5073fa83e0f4207",
+    "url": "/static/js/main.b9e88802.chunk.js"
+  },
+  {
+    "revision": "2ee629b04e005e9509c1",
+    "url": "/static/js/runtime-main.efe862f0.js"
+  }
+]);
--- src/Fly.js
+++ src/Fly.js
@@ -3,13 +3,17 @@ import React, {
 } from 'react';
 import * as THREE from 'three';
 import Colors from './Color';
+import {
+	OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls'
 class Fly extends Component {
 	componentDidMount() {
 		this.getSence();
-		//this.createLights();
+		this.createLights();
 		//this.sea();
-		this.star();
-		this.box();
+		this.star(500);
+		this.control();
+		this.play();
 		this.anima();
 	}
 
@@ -18,31 +22,20 @@ class Fly extends Component {
 		this.stargroup = new THREE.Group();
 		this.scene.add(this.stargroup);
 		this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-		this.renderer = new THREE.WebGLRenderer({
-			antialias: true
-		});
+		this.renderer = new THREE.WebGLRenderer({});
 		this.renderer.setSize(window.innerWidth, window.innerHeight);
 		document.getElementById('container').appendChild(this.renderer.domElement);
+		this.camera.position.z = 5;
+
 
 	}
 	createLights = () => {
-		var hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, .9);
-		var shadowLight = new THREE.DirectionalLight(0xffffff, .9);
-		shadowLight.position.set(150, 350, 350);
-		shadowLight.castShadow = true;
-		shadowLight.shadow.camera.left = -400;
-		shadowLight.shadow.camera.right = 400;
-		shadowLight.shadow.camera.top = 400;
-		shadowLight.shadow.camera.bottom = -400;
-		shadowLight.shadow.camera.near = 1;
-		shadowLight.shadow.camera.far = 1000;
-		shadowLight.shadow.mapSize.width = 2048;
-		shadowLight.shadow.mapSize.height = 2048;
-		this.scene.add(hemisphereLight);
-		this.scene.add(shadowLight);
+		var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+		directionalLight.position.set(0, 1, 0);
+		this.scene.add(directionalLight);
 	}
 	sea = () => {
-		var geom = new THREE.CylinderGeometry(600, 600, 800, 40, 10);
+		var geom = new THREE.CylinderGeometry(2, 2, 4, 40, 10);
 
 		// 创建材质
 		var mat = new THREE.MeshBasicMaterial({
@@ -55,13 +48,13 @@ class Fly extends Component {
 
 		this.scene.add(mesh);
 	}
-	star = () => {
+	star = (starnum) => {
 		var geometry = new THREE.BoxGeometry(100, 100, 100); //创建一个立方体几何对象Geometry
 		var material = new THREE.PointsMaterial({
 			color: 0xffffff,
-			size: 0.1//点对象像素尺寸
+			size: 0.1 //点对象像素尺寸
 		}); //材质对象
-		for (let i = 0; i < 1000; i++) {
+		for (let i = 0; i < starnum; i++) {
 			var points = new THREE.Points(geometry, material); //点模型对象
 
 			this.stargroup.add(points);
@@ -72,20 +65,24 @@ class Fly extends Component {
 		}
 	}
 	starupdate = () => {
-		this.stargroup.children.forEach((onepoint)=>{
-		var flag=Math.random();
-		if(flag>0.5){
-			onepoint.position.x=onepoint.position.x-0.01;
-			if(onepoint.position.x<-1)
-				onepoint.position.x=200;
-		}else
-			{
-				onepoint.position.x=onepoint.position.x+0.01;
-					if(onepoint.position.x<-1)
-						onepoint.position.x=200;
+		this.stargroup.children.forEach((onepoint) => {
+			var flag = Math.random();
+			if (flag > 0.5) {
+				onepoint.position.x = onepoint.position.x - 0.01;
+				if (onepoint.position.x < -1)
+					onepoint.position.x = 200;
+			} else {
+				onepoint.position.x = onepoint.position.x + 0.01;
+				if (onepoint.position.x < -1)
+					onepoint.position.x = 200;
 			}
 
 		});
+
+		var delta = this.clock.getDelta(); //获取时间差
+		this.orbitControls.update(delta); //更新时间
+
+
 	}
 
 
@@ -96,13 +93,48 @@ class Fly extends Component {
 
 	}
 
-	box = () => {
-		var geometry = new THREE.BoxGeometry(100, 100, 100); //创建一个立方体几何对象Geometry
-		var material = new THREE.MeshLambertMaterial({
-			color: Colors.red
-		}); //材质对象Material
-		var mesh = new THREE.Mesh(geometry, material); //网格模型对象Mesh
-		this.scene.add(mesh); //网格模型添加到场景中
+	control = () => {
+		this.orbitControls = new OrbitControls(this.camera, this.renderer.domElement);
+		this.orbitControls.target = new THREE.Vector3(0, 0, 0); //控制焦点
+		this.clock = new THREE.Clock(); //用于更新轨道控制器
+	}
+	play = () => {
+		// 初始化一个侦听器
+		var audioListener = new THREE.AudioListener();
+
+		// 添加该侦听器到相机中
+		this.camera.add(audioListener);
+
+		// 实例化一个音频（环境音）对象
+		var oceanAmbientSound = new THREE.Audio(audioListener);
+
+		// 把该对象添加到场景中去
+		this.scene.add(oceanAmbientSound);
+
+		// 实例化一个加载器
+		var loader = new THREE.AudioLoader();
+
+		// 通过URL加载一个资源
+		loader.load(
+			// 资源链接
+			// 资源加载完成后的回调函数
+			'http://m10.music.126.net/20200820111617/ac68e53298446f629459b6dcde7f2961/ymusic/8f09/50ad/d80a/81fc204c00cf11c89a46581cff73149a.mp3',
+			function(audioBuffer) {
+				// set the audio object buffer to the loaded object
+				oceanAmbientSound.setBuffer(audioBuffer);
+
+				// 播放音频
+				oceanAmbientSound.play();
+			},
+			// 资源加载过程中的回调函数
+			function(xhr) {
+				console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+			},
+			// 资源下载错误的回调函数
+			function(xhr) {
+				console.log('An error happened');
+			}
+		);
 	}
 	render = () => {
 		return (
